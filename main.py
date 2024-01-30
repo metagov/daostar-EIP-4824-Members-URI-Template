0f9fbfd2196e55d5e6ea1e2003947826bcdab360
Rashmi V Abbigeri <41290852+Rashmi-278@users.noreply.github.com>
2024-01-30 23:01:46 +0530

    Update main.py
    Add first param to ask for the number of members

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request  # Import 'request' from flask
 import requests
 
 app = Flask(__name__)
 
-@app.route('/membersURI', methods=['GET'])
+@app.route('/', methods=['GET'])
 def fetch_dao_data():
     # API endpoint
     url = "https://hub.snapshot.org/graphql"
+
+    # Get 'first' parameter from query string, default to 200 if not provided
+    first_param = request.args.get('first', default=200, type=int)
 
     # GraphQL query
     query = """
@@ -22,19 +25,18 @@ def fetch_dao_data():
         "followsWhere2": {
             "space": "lodestarfinance.eth"
         },
-        "first": 200
+        "first": first_param  # Use the parameter from the query string
     }
 
     # Making the POST request
     response = requests.post(url, json={'query': query, 'variables': variables})
 
-    # Check if the request was successful
     if response.status_code == 200:
         # Parsing the response
         data = response.json()
         followers = data['data']['follows']
 
-        # Preparing the formatted JSON with follower IDs and additional information
+        # Preparing the formatted JSON according to DAO URI
         formatted_followers = {
             "members": [{"id": follower["id"], "type": "EthereumAddress"} for follower in followers],
             "@context": {"@vocab": "http://daostar.org/"},
